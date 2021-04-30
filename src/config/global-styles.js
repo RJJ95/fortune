c400b88bcb824d1fc644413d43447800c629b11e
RJJ95 <ricardo.de.vries@hotmail.com>
2021-04-30 18:11:51 +0200

    did font-sizing

--- src/config/global-styles.js
+++ src/config/global-styles.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+html {
+  font-size: 100%;
+}
+
 body {
   margin: 0;
   -webkit-font-smoothing: antialiased;
@@ -12,36 +16,32 @@ h1, h2, h3, h4, h5, h6 {
 }
 
 h1 {
-  font-size: 2em;
+  font-size: 2.5rem;
 }
 
 h2 { 
-  font-size: 1.5em; 
+  font-size: 1.375rem; 
 }
 
 h3 { 
-  font-size: 1.17em; 
+  font-size: 1.25rem; 
 }
 
 h4 { 
-  font-size: 1.12em; 
+  font-size: 0.875rem; 
 }
 
 h5 { 
-  font-size: .83em; 
-}
-
-h6 { 
-  font-size: .75em; 
+  font-size: 0.625rem; 
 }
 
 p {
-  font-size: 1em;
+  font-size: 1rem;
   line-height: 1.8;
 }
 
 button {
-  font-size: 1em;
+  font-size: 1rem;
   padding: 10px;
   border-radius: ${({ theme }) => theme.borderRadius.medium};
   box-shadow: ${({ theme }) => theme.shadows.medium};
@@ -49,7 +49,7 @@ button {
 }
 
 input[type=text] {
-  font-size: 1em;
+  font-size: 1rem;
   padding: 10px;
   border-radius: ${({ theme }) => theme.borderRadius.medium};
   box-shadow: ${({ theme }) => theme.shadows.medium};
