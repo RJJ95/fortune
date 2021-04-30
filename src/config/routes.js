8b77e51e28ea3e718c0f7866fb1c00fce4174794
RJJ95 <ricardo.de.vries@hotmail.com>
2021-04-30 20:08:56 +0200

    routing

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,13 +9,19 @@ import TestingPlayground from "../pages/testing-playground";
 
 // Icons
 import { ReactComponent as Person } from "../assets/icons/person.svg";
+import { ReactComponent as Card } from "../assets/icons/card.svg";
+import { ReactComponent as Archive } from "../assets/icons/archive.svg";
+import { ReactComponent as Ember } from "../assets/icons/ember.svg";
+import { ReactComponent as Graph } from "../assets/icons/graph.svg";
+import { ReactComponent as Gear } from "../assets/icons/gear.svg";
 
 export const routes = {
   home: {
     path: "/",
     exact: true,
     component: Home,
-    name: <Person />,
+    icon: <Person />,
+    name: "Home",
   },
   testingPlayground: {
     path: "/testing-playground",
@@ -29,21 +35,29 @@ export const routes = {
     path: "/payments",
     exact: true,
     component: Payments,
+    icon: <Card />,
+    name: "Payments",
   },
   savings: {
     path: "/savings",
     exact: true,
     component: Savings,
+    icon: <Archive />,
+    name: "Savings",
   },
   stocks: {
     path: "/stocks",
     exact: true,
     component: Stocks,
+    icon: <Graph />,
+    name: "Stocks",
   },
   configuration: {
     path: "/configuration",
     exact: true,
     component: Configuration,
+    icon: <Gear />,
+    name: "Configuration",
   },
   insurance: {
     path: "/insurance",
